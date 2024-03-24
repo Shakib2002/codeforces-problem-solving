c0f1639300d6e5c5ede684bf5689bd93a3b88590
Shakib <shakib123.org@gmail.com>
2024-03-25 01:29:23 +0600

    area of circle

---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define PI 3.141592653
+
+ int main()
+ {
+    double r;
+    cin>>r;
+
+    double area = PI*r*r;
+    cout<<fixed<<setprecision(9)<<area<<endl;
+    
+
+
+  return 0;
+ }
