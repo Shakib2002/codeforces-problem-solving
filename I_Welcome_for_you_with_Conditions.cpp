793df8f8148d0068dcff5c2973aa6be7e7b4567b
Shakib <shakib123.org@gmail.com>
2024-03-25 05:07:10 +0600

    initial

---
+++ I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,12 @@
+/*Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".*/
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<(a>=b ? "Yes" : "No")<<endl; 
+  return 0;
+ }
---
+++ K_Max_and_Min.cpp
@@ -0,0 +1,78 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b, c;
+//     cin >> a >> b >> c;
+    
+//     if (a<b && a<c)
+//     {
+//         cout<<a;
+//     }
+
+//     else if (b<a && b<c)
+//     {
+//         cout<<b;
+//     }
+//     else if (c<a && c<b)
+//     {
+//         cout<<c;
+//     }
+//     cout<<" ";
+
+//     if (a>b && a>c)
+//     {
+//         cout<<a;
+//     }
+
+//     else if (b>a && b>c)
+//     {
+//         cout<<b;
+//     }
+//     else if (c>a && c>b)
+//     {
+//         cout<<c;
+//     }
+//     return 0;
+// }
+
+
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a<=b && a<=c)
+    
+        cout<<a;
+    
+
+    else if (b<=a && b<=c)
+    
+        cout<<b;
+    
+    else if (c<=a && c<=b)
+    
+        cout<<c;
+    
+    cout<<" ";
+
+    if (a>=b && a>=c)
+    
+        cout<<a;
+    
+
+    else if (b>=a && b>=c)
+    
+        cout<<b;
+    
+    else if (c>=a && c>=b)
+
+        cout<<c;
+    
+    return 0;
+}
