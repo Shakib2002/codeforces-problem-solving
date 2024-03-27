13d76b7b73199a46a05328e33ea007f0a1f2d500
Shakib <shakib123.org@gmail.com>
2024-03-28 03:40:10 +0600

    ICPC

--- icpc_sheet2/H_One_Prime.cpp
+++ icpc_sheet2/H_One_Prime.cpp
@@ -2,6 +2,16 @@
 using namespace std;
  int main()
  {
-     
+    int n;
+    cin>>n;
+     for(int i=2; i<=sqrt(n); i++){
+        if(n%i==0){
+            cout<<"NO"<<endl;
+            return 0;
+        }
+     }
+     cout<<"YES"<<endl;
+      
+
   return 0;
  }
